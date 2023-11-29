28659be4b1d118338c09631dd3046a9ffa897bfc
Nishat <tasnim.nt00@gmail.com>
2023-11-30 03:28:38 +0600

    ake responsive for mobile

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
             <h1 className="text-center font-bold text-xl lg:text-4xl text-white pb-5">
               Expense Chart
             </h1>
-            <div className="bg-white rounded-3xl lg:h-[500px] lg:w-[500px] h-96 w-80">
+            <div className="bg-white rounded-3xl lg:h-[500px] lg:w-[500px]">
               <h1 className="text-center font-semibold text-xl lg:text-2xl py-5">
                 Expenses
               </h1>
